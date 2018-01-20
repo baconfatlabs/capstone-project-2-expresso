8411e074ee764574d4328e55f57da4f4682ae323
Eric Wolf <eric@baconfatlabs.com>
2018-01-19 18:21:04 -0600

    First commit

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const path = require('path');
 
 const PORT = process.env.PORT || 4000;
 
-const cors = require('cors');
-app.use( (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 const morgan = require('morgan');
 app.use(morgan('dev'));
 
